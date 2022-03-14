2a05e07a7ce9870e4ea7349a12ca1fb1374afcb7
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-14 14:01:35 -0700

    fixed headers

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", CLIENT_ORIGIN);
+    res.header("Access-Control-Allow-Origin", process.env.CLIENT_ORIGIN);
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
