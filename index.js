b1da4de437d31e09279b0bb88c95985ebbac484b
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-15 08:28:57 -0700

    removing things

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ app.set('port', process.env.PORT )
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, '../frontend/build/index.html')))
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
-})
+// app.use(express.static(path.join(__dirname, '../frontend/build/index.html')))
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+// })
 
 app.use((err, req, res, next) => {
     const statusCode = res.statusCode || 500
