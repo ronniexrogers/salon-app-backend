8ef0912a494b8bd0d7fc599da3440105cbfccff4
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-15 08:04:13 -0700

    setting port

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ app.set('port', process.env.PORT )
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, '../frontend/build/index.html')))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/build'))
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 })
 
 app.use((err, req, res, next) => {
