3a8e100dcf8f38f7404a8fc514ebaf24bbd7b713
ronniexrogers <ronniexrogers@yahoo.com>
2022-03-21 21:44:03 -0700

    changing origin

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((err, req, res, next) => {
 })
 
 app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', 'https://denisse-morales.netlify.app/');
+    res.append('Access-Control-Allow-Origin', '*');
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
